f771b912e496ec798e9535ab682622fa5b3f5503
Matt <matt@crazy-logic.co.uk>
2016-08-05 00:35:31 +0100

    Update InstallProcessing.sh

--- InstallProcessing.sh
+++ InstallProcessing.sh
@@ -1,12 +1,3 @@
-# Install-Processing-on-Ubuntu-16.04
-# a bash script to install Processing 3.1.2 on Ubuntu 16.04
-# based on http://www.artsnova.com/processing/installing-processing-ubuntu-linux-tutorial.html 
-
-#run this file under sudo in the same directory as the 3.1.2 tarball download 
-# if this is the downloads folder do the following. 
-# cd ~/Downloads 
-# ./InstallProcessing.sh
-
 #!/bin/bash          
           echo This script will install Processing-3.1.2-linux64 on Ubuntu 16.04.
 
